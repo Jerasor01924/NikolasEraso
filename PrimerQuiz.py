4ce675991fe2cd3a2ad8776bd68c7fd8d63656ca
Jerasor01924 <53278889+Jerasor01924@users.noreply.github.com>
2019-07-29 16:39:57 -0500

    Create PrimerQuiz.py

---
+++ PrimerQuiz.py
@@ -0,0 +1,22 @@
+'''Primer quiz: El robot se mueve toma la manzana 
+deja un banano y y se devuelvea home
+Autor: Nikolas Eraso'''
+
+#Mover hasta manzana
+print("hola soy reeborg")
+print("voy a hacer una mision")
+repeat 4:
+    move()
+    
+#pause()
+#Tomar manzana y dejar banano
+take("apple")
+put("banana")
+#pause()
+#Devolverse a home
+repeat 2:
+    turn_left()
+#pause()
+repeat 4:
+    move()
+#FIN
